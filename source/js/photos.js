e2b5c9b55475ba59d04336f65a1755ab8262a016
lei2rock <zjpjzdl@qq.com>
2020-04-06 01:46:53 +0800

    Update config

--- source/js/photos.js
+++ source/js/photos.js
@@ -1,4 +1,4 @@
-var imgDataPath = '/photos/photoslist.json'; //图片名称高宽信息json文件路径
+var imgDataPath = '/photos/photoslist.json'; //图片名称尺寸信息json文件路径
 var imgPath = 'https://web-1256060851.file.myqcloud.com/images/photos/';  //图片访问路径
 var imgMaxNum = 50; //图片显示数量
 
